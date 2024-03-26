05e804af1c31eda36bc9db1eea52983c4e077f67
alnandoh <alnandoharsono@gmail.com>
2024-03-26 13:02:24 +0700

    Finished exercise 1

--- src/main/java/com/adepuu/exercises/session6/Exercise1.java
+++ src/main/java/com/adepuu/exercises/session6/Exercise1.java
@@ -1,5 +1,7 @@
 package com.adepuu.exercises.session6;
 
+import java.util.Scanner;
+
 public class Exercise1 {
     /**
      * Write a Java Program to Enter numbers to calculate average and enter 'q' to finish.
@@ -13,6 +15,30 @@ public class Exercise1 {
      * Explanation: print “Invalid input. Please enter a valid number or 'q' to finish.” if user not inserting the expected number or character.
      */
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sum=0,count=0;
+        System.out.println("Enter numbers to calculate average and enter 'q' to finish");
 
+        while(true){
+            String input = scanner.next();
+            if(input.equalsIgnoreCase("q")){
+                break;
+            }
+
+            try{
+                int num= Integer.parseInt(input);
+                sum+=num;
+                count++;
+            }catch(NumberFormatException e){
+                System.out.println("Invalid input. Please enter a valid number or 'q' to finish.");
+            }
+        }
+
+        if(count>0){
+            double average =(double) sum/count;
+            System.out.println("Average:"+average);
+        }else {
+            System.out.println("Please input a number to count the average");
+        }
     }
 }
